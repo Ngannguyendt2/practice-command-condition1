b5e2fa006bd3ca6adf1356035b5357e6cbe6f1a4
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-05 11:00:34 +0700

    up file check age

---
+++ age.js
@@ -0,0 +1,12 @@
+function old() {
+    let old=parseInt(prompt("Your age?"));
+    if(old>=15)
+    {
+        alert("Eligibal");
+    }
+    else
+    {
+        alert("Unconditional");
+    }
+
+}
