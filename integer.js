e52d7b4e96a58d318627871f858605250955bdff
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-05 11:08:09 +0700

    up file check integer

---
+++ integer.js
@@ -0,0 +1,16 @@
+function integ() {
+    let number=prompt("Your number?");
+    if(number>0)
+    {
+        document.write("Bigger zero");
+    }
+    else if(number==0)
+
+    {
+        document.write("Equal zero");
+    }
+    else
+    {
+        document.write("Smaller zero");
+    }
+}
